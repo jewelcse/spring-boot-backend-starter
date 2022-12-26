57856eaa116f74f1ac62b01a1586d436c9cd41b7
jewelcse <jewelcse045@gmail.com>
2022-12-26 12:23:59 +0600

    initiated

---
+++ src/main/java/com/backend/starter/SpringBootBackendStarterApplication.java
@@ -0,0 +1,13 @@
+package com.backend.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/starter/SpringBootBackendStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
