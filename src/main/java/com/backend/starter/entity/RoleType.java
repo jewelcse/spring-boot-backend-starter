ec438cc4bb33ec9831b2c50ad0435ce7e90a7b3c
jewelcse <jewelcse045@gmail.com>
2022-12-26 12:34:00 +0600

    added user, role, profile entity and roleType, gender enum

---
+++ src/main/java/com/backend/starter/entity/RoleType.java
@@ -0,0 +1,7 @@
+package com.backend.starter.entity;
+
+public enum RoleType {
+    ROLE_ADMIN,
+    ROLE_TRAINEE,
+    ROLE_TRAINER
+}
---
+++ src/main/java/com/backend/starter/entity/User.java
@@ -0,0 +1,51 @@
+package com.backend.starter.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    @Size(max = 20)
+    private String username;
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+    private boolean enabled;
+    private boolean nonLocked;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "profile_id")
+    private Profile profile;
+
+
+}
